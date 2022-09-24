abd0cab6cf0a3c623331cc8e66441eb39fc15978
Dhananjay Kumar <djmac@Dhananjays-MacBook-Pro.local>
2022-09-24 19:35:30 +0530

    abc

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,27 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { RouterModule } from '@angular/router';
 
-import { AppModule } from './app/app.module';
+// import { AppModule } from './app/app.module';
+import {bootstrapApplication, BrowserModule} from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { APP_ROUTES } from './app/app-routing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent,{
+
+  providers:[importProvidersFrom(HttpClientModule),
+    importProvidersFrom(BrowserAnimationsModule),
+    importProvidersFrom(RouterModule.forRoot(APP_ROUTES))]
+});
+
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
